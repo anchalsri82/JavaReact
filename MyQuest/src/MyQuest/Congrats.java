30516f0382367c6de035321473c9a2d909ed717c
Anchal Srivastava <anchal.sri82@gmail.com>
2019-09-21 18:06:06 +0800

    Lab 1

---
+++ MyQuest/src/MyQuest/Congrats.java
@@ -0,0 +1,18 @@
+package MyQuest;
+
+public class Congrats {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Congratulations!!!");
+		System.out.println("You are already 50% successful by just SHOWING UP !!!");
+		
+		// Single Line Comment
+		
+		/*
+		 Multiple 
+		 Line
+		 Comment
+		 */
+	}
+}
---
+++ MyQuest/src/MyQuest/MyDream.java
@@ -0,0 +1,13 @@
+package MyQuest;
+import java.util.Scanner;
+
+public class MyDream {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				Scanner sc = new Scanner(System.in);			
+				String a = sc.next();    				// INPUT
+				if(a.contentEquals("Good"))				// PROCESSING
+					System.out.println("Buy car");		// OUTPUT
+	}
+}
